fb9d0f1c39764d2b5eb8b5b18165eaaef1ad5c1d
Anton Zhirkov <an.zhirkov@gmail.com>
2015-10-30 16:50:26 +0300

    comments forms and migrations

--- app/models/suggestion_box/suggestion.rb
+++ app/models/suggestion_box/suggestion.rb
@@ -1,6 +1,6 @@
 module SuggestionBox
   class Suggestion < ActiveRecord::Base
-    # acts_as_commentable class_name: 'SuggestionBox::Comment'
+    acts_as_commentable class_name: 'SuggestionBox::Comment'
     # acts_as_votable
 
     belongs_to :user, class_name: SuggestionBox.author_class || 'User'

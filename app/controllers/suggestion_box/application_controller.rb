9bbc1181fec4e9eac2c1a345adbbeb4026ae9db1
Anton Zhirkov <an.zhirkov@gmail.com>
2015-10-30 14:38:55 +0300

    routes, forms, links

--- app/controllers/suggestion_box/application_controller.rb
+++ app/controllers/suggestion_box/application_controller.rb
@@ -1,6 +1,4 @@
 module SuggestionBox
   class ApplicationController < ::ApplicationController
-    # include SuggestionBox::Engine.routes.url_helpers
-    # helper Rails.application.routes.url_helpers
   end
 end
--- lib/suggestion_box/engine.rb
+++ lib/suggestion_box/engine.rb
@@ -1,7 +1,6 @@
 module SuggestionBox
   class Engine < ::Rails::Engine
     isolate_namespace SuggestionBox
-    # engine_name 'suggestion_box'
     initializer "suggestion_box.url_helpers" do
       ActiveSupport.on_load(:action_controller) do
         helper Rails.application.routes.url_helpers

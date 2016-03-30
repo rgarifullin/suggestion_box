643e1bfbdeae9ebdb7f5b62e801c8916abe22e15
Rinat Garifullin <ringarifullin@gmail.com>
2016-03-30 16:11:35 +0300

    Add sortable helper

--- app/helpers/suggestion_box/suggestions_helper.rb
+++ app/helpers/suggestion_box/suggestions_helper.rb
@@ -1,4 +1,20 @@
 module SuggestionBox
   module SuggestionsHelper
+    def sortable(column, title = nil)
+      title ||= column.titleize
+      css_class = column == sort_column ? "current #{sort_direction}" : nil
+      direction = (column == params[:sort] && params[:direction] == 'asc') ? 'desc' : 'asc'
+      link_to title, { sort: column, direction: direction }, { class: css_class }
+    end
+
+    private
+
+    def sort_column
+      params[:sort] ? params[:sort] : 'title'
+    end
+
+    def sort_direction
+      %w(wasc desc).include?(params[:direction]) ? params[:direction] : 'asc'
+    end
   end
 end
